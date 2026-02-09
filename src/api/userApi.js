9160d34ddd42e9adb0921c7770463b4c0e1e7b01
raviteja7893055964 <gunanaga.b@gmail.com>
2026-02-09 20:26:10 +0530

    Replaced json-server with MockAPI

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3001/users";
+// const API_URL = "http://localhost:3001/users";
+const API_URL = "https://6989f0d6c04d974bc6a0e4cc.mockapi.io/users";
 
 export const getUsers = () => axios.get(API_URL);
 export const createUser = (data) => axios.post(API_URL, data);
